50c95f90b24109aa9437be17ca0231fb0c9cede1
An Vu <anvutien99@gmail.com>
2019-06-15 23:41:24 +0100

    return the sum of all of the positives numbers in an array

---
+++ src/kata/kyu8/SumOfPositive/Positive.java
@@ -0,0 +1,22 @@
+package kata.kyu8.SumOfPositive;
+
+public class Positive{
+
+	  public static int sum(int[] arr) {
+	  
+	  int sum = 0;
+	  
+	    for (int i = 0; i < arr.length; i++) {
+	    
+	      if (arr[i] > 0) {
+	      
+	        sum += arr[i];
+	      
+	      }
+	    
+	    }
+	    
+	    return sum;
+	  }
+
+	}
