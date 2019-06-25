40a3e7ae212c1e6b86a69eec822fa7f3f8df88fe
An Vu <anvutien99@gmail.com>
2019-06-25 23:05:49 +0100

    Calculate factorial for a given input.

---
+++ src/kata/kyu7/Factorial/Factorial.java
@@ -0,0 +1,18 @@
+public class Factorial {
+
+  public int factorial(int n) {
+  
+    if (n < 0 || n > 12) {
+    
+      throw new IllegalArgumentException();
+    
+    }
+  
+    if (n == 0)    
+      return 1;   
+    
+    else    
+      return(n * factorial(n-1));    
+       
+  }
+}
