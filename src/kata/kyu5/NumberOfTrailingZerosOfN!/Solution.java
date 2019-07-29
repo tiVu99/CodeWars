82e5e21a6939823b8687de3a60839a5a28bf10d7
An Vu <anvutien99@gmail.com>
2019-07-29 07:06:22 +0100

    Return the number of trailing zeros in a factorial of a given number.

---
+++ src/kata/kyu5/NumberOfTrailingZerosOfN!/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+  public static int zeros(int n) {
+  
+    int count = 0;  
+    
+      for (int i = 5; n / i >= 1; i *= 5) {
+            count += n / i; 
+      }
+      
+  return count; 
+  }
+}
